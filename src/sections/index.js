9306cd74e1f7596290fd05a111f5f1c2c34372bc
Thusala Piyarisi <thusalapi@gmail.com>
2024-05-24 16:12:40 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    CustomerReviews,
+    Footer,
+    SpecialOffer,
+    Subscribe,
+    }
